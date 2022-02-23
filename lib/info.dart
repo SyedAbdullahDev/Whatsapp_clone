5505c5a03f3ba1024f2b3fe1c523c78a5bc100f5
S-Abbdullah <98264416+S-Abbdullah@users.noreply.github.com>
2022-02-23 23:09:27 +0500

    ,

---
+++ lib/info.dart
@@ -0,0 +1,71 @@
+const info = [
+  {
+    'name': 'Abuzar',
+    'message': 'I am great man!',
+    'time': '2:25 pm',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Nouman',
+    'message': 'I am great man!',
+    'time': '1:03 pm',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Akbar',
+    'message': 'I am great man!',
+    'time': '12:06 pm',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Mom',
+    'message': 'I am great man!',
+    'time': '10:00 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Dad',
+    'message': 'I am great man!',
+    'time': '9:53 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Abdullah',
+    'message': 'I am great man!',
+    'time': '7:25 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Ahmad',
+    'message': 'I am great man!',
+    'time': '6:02 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Sikandar',
+    'message': 'I am great man!',
+    'time': '4:56 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Ali',
+    'message': 'I am great man!',
+    'time': '1:00 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+];
+
+const messages = [
+  {"isMe": false, "text": "Hey What is up with you!!", "time": "10:00 am"},
+  {"isMe": true, "text": "im fine,wbu?", "time": "11:00 am"},
+  {"isMe": false, "text": "I am great man!", "time": "11:01 am"},
+];
